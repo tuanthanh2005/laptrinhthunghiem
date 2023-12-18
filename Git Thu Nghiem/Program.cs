0fe756a688fe69e62f8ab9d516a19400cecbefe4
tranthanhtuan12cb6 <tranthanhtua12cb6@gmail.com>
2023-12-18 20:23:23 +0700

    Core 1

---
+++ Git Thu Nghiem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Thu_Nghiem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Hello Word Git Thử NGhiệm !!!!");
+            Console.ReadKey();
+        }
+    }
+}
